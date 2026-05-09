9c6003738d40447e2704db215f34354b53ed5c55
rongxin <dengrongxin@hotmail.com>
2026-05-09 23:47:30 +0800

    Fix URL ordering: move create/ before <slug:slug>/ to fix 404

--- apps/listings/urls.py
+++ apps/listings/urls.py
@@ -6,9 +6,9 @@ app_name = 'listings'
 urlpatterns = [
     path('', views.ListingListView.as_view(), name='list'),
     path('category/<slug:slug>/', views.ListingByCategoryView.as_view(), name='category'),
-    path('<slug:slug>/', views.ListingDetailView.as_view(), name='detail'),
     path('create/', views.ListingCreateView.as_view(), name='create'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    path('<slug:slug>/', views.ListingDetailView.as_view(), name='detail'),
     path('<slug:slug>/edit/', views.ListingUpdateView.as_view(), name='edit'),
     path('<slug:slug>/delete/', views.ListingDeleteView.as_view(), name='delete'),
-    path('search/', views.SearchView.as_view(), name='search'),
 ]
